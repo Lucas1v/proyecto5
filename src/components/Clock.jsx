f796d536a66bb4e6a839f9bbd6674b9cc1a63787
lucas <lucas1vinet@gmail.com>
2021-09-17 18:58:28 -0300

    second-finish

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -8,6 +8,7 @@ const Clock =({sessionChange, breakeChange, session, breake, refresh, touch, off
     const [segundos, setSegundos] = useState(0);
     const [valor, setValor] = useState(false);
     const [name, setName] = useState("session");
+    let audio = document.getElementById("beep");
     useEffect(() => {
         let interval = null;
         if(actualTime){
@@ -21,41 +22,48 @@ const Clock =({sessionChange, breakeChange, session, breake, refresh, touch, off
                 setSegundos(0)
                 }
         }
+        if(minutos === 0 && segundos === 0){
+            audio.play();
+            setTimeout(() => {
+                audio.pause()
+            }, 1000);
+        }
+        console.log(segundos)
         if(valor){
             offTouch()
             interval = setInterval(() => {
                 if(actualTime){
                     if(minutos === 0 && segundos === 0){
-                    setActualTime(prevSession => !prevSession);
-                    setMinutos(breake);
-                    if(segundos === 0){
-                        setMinutos(prevTime => prevTime - 1);
-                        setSegundos(prevTime => prevTime + 59);
+                        setActualTime(prevSession => !prevSession);
+                        setMinutos(breake + 1);
+                        if(segundos === 0){
+                            setSegundos(prevTime => prevTime + 59);
+                            setMinutos(prevTime => prevTime - 1);
+                        }else{
+                            setSegundos(prevTime => prevTime - 1);
+                        }   
                     }else{
-                        setSegundos(prevTime => prevTime - 1);
-                    }
-                    }else{
-                    if(segundos === 0){
-                        setMinutos(prevTime => prevTime - 1);
-                        setSegundos(prevTime => prevTime + 59);
-                    }else{
-                        setSegundos(prevTime => prevTime - 1);
-                    }
+                        if(segundos === 0){
+                            setSegundos(prevTime => prevTime + 59);
+                            setMinutos(prevTime => prevTime - 1);
+                        }else{
+                            setSegundos(prevTime => prevTime - 1);
+                        }
                     }
                 }else{
                     if(minutos === 0 && segundos === 0){
                         setActualTime(prevSession => !prevSession);
-                        setMinutos(session);
+                        setMinutos(session + 1);
                         if(segundos === 0){
-                            setMinutos(prevTime => prevTime - 1);
                             setSegundos(prevTime => prevTime + 59);
+                            setMinutos(prevTime => prevTime - 1);
                         }else{
                             setSegundos(prevTime => prevTime - 1);
                         }
                         }else{
                         if(segundos === 0){
-                            setMinutos(prevTime => prevTime - 1);
                             setSegundos(prevTime => prevTime + 59);
+                            setMinutos(prevTime => prevTime - 1);
                         }else{
                             setSegundos(prevTime => prevTime - 1);
                         }
@@ -68,15 +76,18 @@ const Clock =({sessionChange, breakeChange, session, breake, refresh, touch, off
         if(actualTime){
             setName("Session")
         }else{
-            setName("break")
+            setName("Break")
         }
         return () => {clearInterval(interval)}
-    }, [segundos, minutos, session, breake, actualTime, valor, touch, name, offTouch])
-
+    }, [segundos, minutos, session, breake, actualTime, valor, touch, name, offTouch, audio])
     const handleRefresh = () => {
         setValor(false);
         setSegundos(0);
         setActualTime(true);
+        setMinutos(session)
+        audio.pause();
+        audio.currentTime = 0;
+        setName("Session")
     };
     return (
         <div id="reloj">
@@ -87,14 +98,14 @@ const Clock =({sessionChange, breakeChange, session, breake, refresh, touch, off
             </div>
             <div id="completo">
                 <div className="third">
-                    <div className=" fa fa-arrow-down fa-2x derecha div" id="break-decrement" onClick={() => breakeChange(-1, !valor)}></div>
+                    <div className=" fa fa-arrow-down fa-2x derecha div" id="break-decrement" onClick={() => breakeChange(-1, !valor, false)}></div>
                     <span className="derecha" id="break-length">{breake}</span>
-                    <div className=" fa fa-arrow-up fa-2x derecha div" id="break-increment" onClick={() => breakeChange(+1, !valor)}></div>
+                    <div className=" fa fa-arrow-up fa-2x derecha div" id="break-increment" onClick={() => breakeChange(+1, !valor, true)}></div>
                 </div>
                 <div className="third" id="sesion">
-                    <div className=" fa fa-arrow-down fa-2x derecha div" id="session-decrement" onClick={() => sessionChange(-1, !valor)}></div>
+                    <div className=" fa fa-arrow-down fa-2x derecha div" id="session-decrement" onClick={() => sessionChange(-1, !valor, false)}></div>
                     <span className="derecha" id="session-length">{session}</span>
-                    <div className=" fa fa-arrow-up fa-2x derecha div" id="session-increment" onClick={() => sessionChange(+1, !valor)}></div>
+                    <div className=" fa fa-arrow-up fa-2x derecha div" id="session-increment" onClick={() => sessionChange(+1, !valor, true)}></div>
                 </div>
             </div>
             <div className="forth">
@@ -103,8 +114,9 @@ const Clock =({sessionChange, breakeChange, session, breake, refresh, touch, off
             </div>
             <div className="fifth">
             <div id="start_stop" className={`fa fa-2x ${(!valor?"fa-play": "fa-pause")} div`} onClick={() => setValor(!valor)}></div>
-            <div id="reset" className=" fa fa-refresh fa-2x  div" onClick={() => {return refresh(), handleRefresh()}}></div>
+            <div id="reset" className=" fa fa-refresh fa-2x  div" onClick={() => {refresh(); handleRefresh()}}></div>
             </div>
+            <audio id="beep" preload="auto" src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"></audio>
         </div>
     )
 }
@@ -116,18 +128,20 @@ const mapStateToPops = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-    sessionChange(o, play) {
+    sessionChange(o, play, up) {
         dispatch({
            type: "CSESSION",
            o: o,
-           play: play
+           play: play,
+           up: up
         })
     },
-    breakeChange(o, play) {
+    breakeChange(o, play, up) {
         dispatch({
            type: "CBREAKE",
            o: o,
-           play: play
+           play: play,
+           up: up
         })
     },
     refresh(){
--- src/store.js
+++ src/store.js
@@ -10,11 +10,23 @@ const reducerClock = (state = initialState, action) =>{
     switch(action.type){
         case "CSESSION":
             if(action.play){
-                if(state.session !== 1){
-                    return {
-                        ...state,
-                        session: state.session + action.o,
-                        touch: true
+                if(state.session < 60){
+                    if(!action.up){
+                        if(state.session > 1){
+                            return {
+                                ...state,
+                                session: state.session + action.o,
+                                touch: true
+                            }
+                        }else{
+                            return state
+                        }
+                    }else{
+                        return {
+                            ...state,
+                            session: state.session + action.o,
+                            touch: true
+                        }
                     }
                 }else{
                     return state
@@ -24,10 +36,23 @@ const reducerClock = (state = initialState, action) =>{
             }
         case "CBREAKE":
             if(action.play){
-                if(state.breake !== 1){
-                    return{
-                    ...state,
-                    breake: state.breake + action.o,
+                if(state.breake < 60){
+                    if(!action.up){
+                        if(state.breake > 1){
+                            return {
+                                ...state,
+                                breake: state.breake + action.o,
+                                touch: true
+                            }
+                        }else{
+                            return state
+                        }
+                    }else{
+                        return {
+                            ...state,
+                            breake: state.breake + action.o,
+                            touch: true
+                        }
                     }
                 }else{
                     return state
@@ -40,6 +65,8 @@ const reducerClock = (state = initialState, action) =>{
                 ...state,
                 touch: false
             }
+        case "REFRESH":
+            return state = initialState
         default:
             return initialState;
         }
